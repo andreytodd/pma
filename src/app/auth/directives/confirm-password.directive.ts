a5db8fc9effd8b4cc86af1dc317c29dbf55f4581
andreytodd <ermolaev@seznam.cz>
2023-03-27 20:57:57 +0200

    Auto-generated commit [ci skip]

--- src/app/auth/directives/confirm-password.directive.ts
+++
@@ -1,9 +0,0 @@
-import {ValidationErrors, ValidatorFn} from "@angular/forms";
-import {AbstractControl} from "@angular/forms";
-
-export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const password = control.get('password');
-  const confirmPassword = control.get('confirmPassword');
-
-  return password && confirmPassword && password.value !== confirmPassword.value ? { passwordMismatch: true } : null;
-};
